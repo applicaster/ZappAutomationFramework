566729f76f345df77dd9d4ad6a7ac4da7618024f
Boaz Warshawsky <b.warshawsky@applicaster.com>
2020-04-18 22:33:21 +0300

    Add framework setup script

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# install carthage
+if [[ $1 == "local" ]]; then
+    sudo brew install carthage
+fi
+
+# install appium server cli
+if [[ $1 == "local" ]]; then
+    sudo npm install -g appium@1.16.0 --unsafe-perm=true --allow-root
+    npm install wd
+fi
+
+# install external python modules
+sudo pip install -r requierments.txt
