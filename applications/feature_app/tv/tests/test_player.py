d66b4a80c0f3825809ad02c8bac5a836308ea9a1
Boaz Warshawsky <b.warshawsky@applicaster.com>
2020-04-22 15:48:29 +0300

    comment out player tests

--- applications/feature_app/tv/tests/test_player.py
+++ applications/feature_app/tv/tests/test_player.py
@@ -1,20 +1,20 @@
-
-import pytest
-
-from src.automation_manager.automation_manager import automation_driver
-from src.base_test import BaseTest, PRINT
-from src.global_defines import RemoteControlKeys
-
-
-class PlayerTest(BaseTest):
-    @pytest.mark.usefixtures('automation_driver')
-    def test_verify_vod_streaming(self):
-        PRINT('Step 1: Start playing "video_feed" vod item from the first component')
-        self.driver.send_keys(RemoteControlKeys.ENTER)
-        timeout = 10
-        PRINT('     Step 1.1: Wait %s seconds until the streaming will start' % timeout)
-        self.driver.wait(timeout)
-
-        PRINT('Step 2: Verify streaming is playing correctly')
-        self.building_blocks.screens['player_screen'].verify_stream_is_playing()
-        PRINT('     Step 2.1: Streaming is playing correctly')
+#
+# import pytest
+#
+# from src.automation_manager.automation_manager import automation_driver
+# from src.base_test import BaseTest, PRINT
+# from src.global_defines import RemoteControlKeys
+#
+#
+# class PlayerTest(BaseTest):
+#     @pytest.mark.usefixtures('automation_driver')
+#     def test_verify_vod_streaming(self):
+#         PRINT('Step 1: Start playing "video_feed" vod item from the first component')
+#         self.driver.send_keys(RemoteControlKeys.ENTER)
+#         timeout = 10
+#         PRINT('     Step 1.1: Wait %s seconds until the streaming will start' % timeout)
+#         self.driver.wait(timeout)
+#
+#         PRINT('Step 2: Verify streaming is playing correctly')
+#         self.building_blocks.screens['player_screen'].verify_stream_is_playing()
+#         PRINT('     Step 2.1: Streaming is playing correctly')
