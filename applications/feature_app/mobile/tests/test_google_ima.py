82132d29784c3384d66a6900b8e72bf37d78374a
Boaz Warshawsky <b.warshawsky@applicaster.com>
2020-06-29 08:12:48 +0300

    feat(IMA): Make IMA VMAP test to pass on android mobile

--- applications/feature_app/mobile/tests/test_google_ima.py
+++ applications/feature_app/mobile/tests/test_google_ima.py
@@ -28,9 +28,12 @@ VMAP_POST_ROLL_URL = 'https://pubads.g.doubleclick.net/gampad/ads?slotname=/1243
                      'vpos=postroll&pod=3&ppos=1&lip=true&min_ad_duration=0&max_ad_duration=30000&vrid=6256&video' \
                      '_doc_id=short_onecue&cmsid=496&kfa=0&tfcd=0'
 
-VMAP_VOD_ITEM_NAME = 'vod_with_play_next' if Configuration.get_instance().platform_type() == PlatformType.ANDROID else 'Id2'
-VAST_VOD_ITEM_NAME = 'm3u8_vod' if Configuration.get_instance().platform_type() == PlatformType.ANDROID else 'Id1'
 VMAP_ADV_URL = 'https://assets-secure.applicaster.com/qa/zapp_qa/automation/advertising/pre_mid_post_roll_vmap.xml'
+
+VMAP_VOD_ITEM_NAME = 'vod_with_play_next'
+VAST_VOD_ITEM_NAME = 'm3u8_vod'
+
+PLATFORM = Configuration.get_instance().platform_type()
 
 
 class GoogleInteractiveMediaAdsTests(BaseTest):
@@ -47,15 +50,21 @@ class GoogleInteractiveMediaAdsTests(BaseTest):
         element = self.driver.find_element_by_text(VAST_VOD_ITEM_NAME)
         element.click()
 
-        PRINT('Step 3: Pass the presented pre hook screen with success, in order to watch the video')
-        PRINT('     Step 3.1: Verify that the demo pre hook screen is presented')
-        pre_hook.verify_in_screen(retries=3)
-        PRINT('     Step 3.2: Dismiss the pre hook screen with success')
-        pre_hook.enter_with_success()
+        if Configuration.get_instance().platform_type() == PlatformType.IOS:
+            PRINT('Step 3: Pass the presented pre hook screen with success, in order to watch the video')
+            PRINT('     Step 3.1: Verify that the demo pre hook screen is presented')
+            pre_hook.verify_in_screen(retries=3)
+
+            PRINT('     Step 3.2: Dismiss the pre hook screen with success')
+            pre_hook.enter_with_success()
 
         PRINT('Step 4: Verify that a VAST pre-roll adv is presented before the video start playing')
         pre_roll_unit_id = \
+            'https://pubads.g.doubleclick.net/pcs/click?xai=AKAOjssEaTazxUzioRp_opiQvL6ll0nxp7L6BFR21Rbl5u7N3ekgqQEx12OLsjZPcCgrfAJcTgFj6GQIOO2wd0Iet7LNxsPiiVL-rTBKpdYKu9mT8JmZCLiGGfz1L9DCSO9SsQcsVgJ9CEuGJ7TpO2a0XREwrvxPmWC8x5Jo-cDAwJvkcMeb_9lmBAKk7IhXr8VUyo3zwqUTgtI3s2WXGo73N92DAiqBfEHJhUyr_83p0veAhPE7v4AY1itOhZ0Lgc__u2-_61VZESybCCTpk_uViDMB4pzkTg&sai=AMfl-YQnEMttDV5oCIaH_X_MEeQACtNInh2lo6uiaGK7cuCHY3yu-S4hwe2Db5y4hdMvVDgY0AtdSyhTLvoTcEhNYP5bi3VXkZmkA3_kttEhybvSrwk8JgS48xkmHsAsnIOlVM3on4eddexfSsD-gj88ZnijleNgB5OaRoyb&sig=Cg0ArKJSzGfhhZLS4nJ6&adurl=https://developers.google.com/interactive-media-ads/docs/vastinspector_dual'
+        if PLATFORM == PlatformType.IOS:
+            pre_roll_unit_id = \
             'https://pubads.g.doubleclick.net/gampad/ads?sz=640x480&iu=/124319096/external/single_ad_samples&ciu_szs=300x250&impl=s&gdfp_req=1&env=vp&output=vast&unviewed_position_start=1&cust_params=deployment%3Ddevsite%26sample_ct%3Dredirectlinear&correlator='
+
         element = self.driver.find_element_by_text(pre_roll_unit_id, 20)
         Logger.get_instance().log_assert(element, 'Test failed displaying video pre roll adv with id: "%s"' % pre_roll_unit_id)
         PRINT('Test verified successfully pre-roll video adv with id "%s"' % pre_roll_unit_id)
@@ -72,7 +81,7 @@ class GoogleInteractiveMediaAdsTests(BaseTest):
 
     def wait_until_adv_is_gone(self, unit_id, timeout=20):
         for i in range(timeout):
-            element = self.driver.find_element_by_text(unit_id)
+            element = self.driver.find_element_by_accessibility_id(unit_id)
             if element is None:
                 return True
             self.driver.wait(1)
@@ -81,7 +90,8 @@ class GoogleInteractiveMediaAdsTests(BaseTest):
     def verify_adv(self, step, section, display_timeout, dismiss_timeout, expected_unit_id):
         step = str(step)
         PRINT('Step %s: Verify playing of VMAP %s with id %s' % (step, section, expected_unit_id))
-        element = self.driver.find_element_by_text(VMAP_ADV_URL, display_timeout)
+        element = self.driver.find_element_by_accessibility_id(VMAP_ADV_URL, display_timeout)
+
         Logger.get_instance().log_assert(element, 'Test failed displaying VMAP %s with id %s' % (section, expected_unit_id))
         PRINT('     Step %s.2: Wait for the %s to complete' % (step, section))
         is_gone = self.wait_until_adv_is_gone(VMAP_ADV_URL, dismiss_timeout)
@@ -99,13 +109,17 @@ class GoogleInteractiveMediaAdsTests(BaseTest):
         PRINT('Step 2: Press on item "%s" element on screen' % VMAP_VOD_ITEM_NAME)
         element = self.driver.find_element_by_text(VMAP_VOD_ITEM_NAME)
         element.click()
-        PRINT('     Step 2.1: Wait for pre hook and dismiss it')
-        self.driver.wait(2)
-        pre_hook.enter_with_success()
+        if Configuration.get_instance().platform_type() == PlatformType.IOS:
+            PRINT('     Step 2.1: Wait for pre hook and dismiss it')
+            self.driver.wait(2)
+            pre_hook.enter_with_success()
 
         self.verify_adv(3, 'pre-roll', 20, 15, VMAP_PRE_ROLL_URL)
         self.verify_adv(4, 'mid-roll', 30, 15, VMAP_MID_ROLL_URL)
-        self.verify_adv(5, 'post-roll', 30, 15, VMAP_POST_ROLL_URL)
+        self.verify_adv(5, 'post-roll', 45, 15, VMAP_POST_ROLL_URL)
+
+        PRINT('Step 3: Verify that the app navigated back to GridScreen after playing the post role')
+        grid_screen.verify_in_screen(retries=10)
 
     def shortDescription(self, test_name) -> str:
         if test_name == 'test_verify_playing_vast_adv_from_json_feed':
