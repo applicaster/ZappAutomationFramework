4845b80026424c00b105f8f35483c8d445a6b1b9
Francois roland <f.roland@applicaster.com>
2020-09-15 17:26:28 +0200

    try splitting tests in different files

--- applications/feature_app/mobile/tests/test_advertising_once.py
+++ applications/feature_app/mobile/tests/test_advertising_once.py
@@ -15,7 +15,7 @@ ADVERTISING_SCREEN_ANDROID_SCREEN_LOAD_TIMEOUT = 10
 PLATFORM = Configuration.get_instance().platform_type()
 
 
-class AdvertisingTests(BaseTest):
+class AdvertisingOnceTests(BaseTest):
     def verify_banner(self, step, unit_id, banner_type):
         interstitial_screen = self.building_blocks.screens[INTERSTITIAL_SCREEN]
         PRINT('Step %s: Verify %s banner with id %s' %
@@ -31,12 +31,12 @@ class AdvertisingTests(BaseTest):
     @pytest.mark.qb_android_mobile_nightly
     @pytest.mark.qb_ios_mobile_nightly
     @pytest.mark.usefixtures('automation_driver')
-    def test_verify_interstitial_and_banners(self):
-        interstitial_screen = self.building_blocks.screens[INTERSTITIAL_SCREEN]
+    def test_verify_interstitial_once(self):
+        interstitial_once_screen = self.building_blocks.screens[INTERSTITIAL_ONCE_SCREEN]
         advertising_screen = self.building_blocks.screens[ADVERTISING_SCREEN]
 
-        PRINT('Step 1: Navigate to "%s" screen' % INTERSTITIAL_SCREEN)
-        interstitial_screen.navigate()
+        PRINT('Step 1: Navigate to "%s" screen' % INTERSTITIAL_ONCE_SCREEN)
+        interstitial_once_screen.navigate()
 
         PRINT('Step 2: Verify that the interstitial screen is presented')
         advertising_screen.verify_interstitial_is_displaying()
@@ -45,22 +45,31 @@ class AdvertisingTests(BaseTest):
         advertising_screen.close_interstitial()
 
         PRINT('Step 4: Verify that we reached the actual screen behind the interstitial')
-        interstitial_screen.verify_in_screen()
-        if PLATFORM == PlatformType.ANDROID:
-            PRINT('     Step 4.1: Start waiting for advertising screen to load')
-            self.driver.wait(ADVERTISING_SCREEN_ANDROID_SCREEN_LOAD_TIMEOUT)
-            PRINT('     Step 4.2: Finished waiting for advertising screen to load')
-
+        interstitial_once_screen.verify_in_screen()
         item_name = 'vod_0'
         PRINT('     Step 4.1: Verify that item "%s" from the data of the screen is displaying' % item_name)
         Logger.get_instance().log_assert(
-            interstitial_screen.search_for_item_by_text(
-                item_name, retries=5), '"%s" item not found in the screen' % item_name
+            interstitial_once_screen.search_for_item_by_text(
+                item_name), '"%s" item not found in the screen' % item_name
         )
 
-        # self.verify_banner(5, '/19489716/smartbanner_test', 'smart')
-        self.verify_banner(6, '/5644/es.lasestrellas.app/home', 'standard')
-        self.verify_banner(7, '/5644/es.lasestrellas.app/secciones', 'box')
+        PRINT('Step 5: Navigate back, in order to open the "%s" screen once again' %
+              INTERSTITIAL_ONCE_SCREEN)
+        self.building_blocks.navigation_bar.press_back_button()
+        PRINT('     Step 5.1: Navigate again the "%s" screen' %
+              INTERSTITIAL_ONCE_SCREEN)
+        interstitial_once_screen.navigate()
+        PRINT('     Step 5.2: Wait 3 seconds to see if the interstitial did show')
+        advertising_screen.verify_interstitial_is_not_displaying()
+        PRINT('     Step 5.3: Verify we reached the "%s" screen and that id did load correctly' %
+              INTERSTITIAL_ONCE_SCREEN)
+        interstitial_once_screen.verify_in_screen()
+        item_name = 'vod_0'
+        PRINT('     Step 5.4: Verify that item "%s" from the data of the screen is displaying' % item_name)
+        Logger.get_instance().log_assert(
+            advertising_screen.search_for_item_by_text(
+                item_name, retries=3), '"%s" item not found in the screen' % item_name
+        )
 
     def shortDescription(self, test_name) -> str:
         if test_name == 'test_verify_interstitial_and_banners':
